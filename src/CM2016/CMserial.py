16f05125ed4ebd6647d7e33375e9159da2fffc75
Michael Wahler <michael.s.wahler@gmail.com>
2018-11-25 13:37:23 +0100

    moved CM2016 serial code to a separate file

---
+++ src/CM2016/CMserial.py
@@ -0,0 +1,58 @@
+"""Reads raw data from the CM2016 battery charger over USB"""
+
+__author__ = "Michael Wahler"
+__copyright__ = "Copyright 2018, Michael Wahler"
+__license__ = "GPLv3"
+__version__ = "0.1"
+__status__ = "Prototype"
+
+import time
+import serial
+import ConfigParser
+
+
+def read_CM2016 ():
+	config = ConfigParser.ConfigParser()
+	config.read('CM2016.ini')
+
+	# configure the serial connections (the parameters differs on the device you are connecting to)
+	ser = serial.Serial(
+		port=config.get('USB','port'),
+		baudrate=19200,
+		parity=serial.PARITY_NONE,
+		stopbits=serial.STOPBITS_ONE,
+		bytesize=serial.EIGHTBITS
+	)
+
+	# print ("ser.isOpen()? {0}".format(ser.isOpen()))
+
+	TIMEOUT_SECONDS=3
+
+	try:
+		timeout = 0
+		timed_out = True
+		while timeout < TIMEOUT_SECONDS:
+
+			while ser.inWaiting() > 0:
+				buffer = ser.read(127) # TODO buffer is can be a string or byte array, depending on the Python version
+
+				# print ("{0} character(s) read".format(len(buffer)))
+
+				if (len(buffer) == 127):
+					ser.close()
+					return buffer
+					timed_out = False
+			time.sleep(1)
+			timeout += 1
+
+		if timed_out:
+			print ("Timed out after {0} seconds.".format(timeout))
+			ser.close ()
+			raise TimeoutError ("The connection to {0} timed out after {1} s.".format (config.get('USB','port'), timeout))
+
+	except Exception as x:
+		print (str(x))
+		ser.close()
+		raise x
+
+	
--- src/main.py
+++ src/main.py
@@ -8,50 +8,15 @@ __license__ = "GPLv3"
 __version__ = "0.1"
 __status__ = "Prototype"
 
-import time
-import serial
-import ConfigParser
-
 from CM2016 import CM2016
-
-
-config = ConfigParser.ConfigParser()
-config.read('CM2016.ini')
-
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-    port=config.get('USB','port'),
-    baudrate=19200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
-)
-
-# print ("ser.isOpen()? {0}".format(ser.isOpen()))
-
-TIMEOUT_SECONDS=3
+from CM2016 import CMserial
 
 try:
-	timeout = 0
-	timed_out = True
-	while timeout < TIMEOUT_SECONDS:
-
-		while ser.inWaiting() > 0:
-        		buffer = ser.read(127) # TODO buffer is can be a string or byte array, depending on the Python version
-
-			# print ("{0} character(s) read".format(len(buffer)))
-
-			if (len(buffer) == 127):
-				cm2016 = CM2016.CM2016 (buffer)
-				cm2016.print_me ()
-				timed_out = False
-		time.sleep(1)
-		timeout += 1
-	
-	if timed_out:
-		print ("Timed out after {0} seconds.".format(timeout))
-
+	buffer = CMserial.read_CM2016 ()
+	cm = CM2016.CM2016 (buffer)
+	cm.print_me()
 except Exception as x:
 	print (str(x))
 
-ser.close()
+
+
