1f363f52cfc45276528b799c873ca0d01f6839ec
Admin <Admin@192.168.4.163>
2020-03-22 08:27:44 +0100

    implementing suggestions by DeepCode

--- src/CM2016/CMserial.py
+++ src/CM2016/CMserial.py
@@ -7,8 +7,8 @@ __version__ = "0.1"
 __status__ = "Prototype"
 
 import time
-import serial
 import ConfigParser
+import serial
 
 
 def read_CM2016 ():
@@ -17,42 +17,40 @@ def read_CM2016 ():
 
 	# configure the serial connections (the parameters differs on the device you are connecting to)
 	ser = serial.Serial(
-		port=config.get('USB','port'),
-		baudrate=19200,
-		parity=serial.PARITY_NONE,
-		stopbits=serial.STOPBITS_ONE,
-		bytesize=serial.EIGHTBITS
+		port = config.get('USB', 'port'),
+		baudrate = 19200,
+		parity = serial.PARITY_NONE,
+		stopbits = serial.STOPBITS_ONE,
+		bytesize = serial.EIGHTBITS
 	)
 
 	# print ("ser.isOpen()? {0}".format(ser.isOpen()))
 
-	TIMEOUT_SECONDS=3
+	TIMEOUT_SECONDS = 3
 
 	try:
 		timeout = 0
-		timed_out = True
+
 		while timeout < TIMEOUT_SECONDS:
 
 			while ser.inWaiting() > 0:
-				buffer = ser.read(127) # TODO buffer is can be a string or byte array, depending on the Python version
+				buffer = ser.read(127)  # TODO buffer is can be a string or byte array, depending on the Python version
 
 				# print ("{0} character(s) read".format(len(buffer)))
 
-				if (len(buffer) == 127):
+				if 127 == len(buffer):
 					ser.close()
 					return buffer
-					timed_out = False
+
 			time.sleep(1)
 			timeout += 1
 
-		if timed_out:
-			#print ("Timed out after {0} seconds.".format(timeout))
-			ser.close ()
-			raise Exception ("The connection to {0} timed out after {1} s.".format (config.get('USB','port'), timeout))
+		# print ("Timed out after {0} seconds.".format(timeout))
+		ser.close ()
+		raise Exception ("The connection to {0} timed out after {1} s.".format (config.get('USB', 'port'), timeout))
 
 	except Exception as x:
-		#print (str(x))
+		# print (str(x))
 		ser.close()
 		raise x
-
 	
--- src/mysql.py
+++ src/mysql.py
@@ -6,43 +6,48 @@ __license__ = "GPLv3"
 __version__ = "0.2"
 __status__ = "Prototype"
 
-import MySQLdb
 import ConfigParser
+import MySQLdb
 
 config = ConfigParser.ConfigParser()
 config.read('CM2016.ini')
 
-USERNAME=config.get('MySQL','username')
-PW=config.get('MySQL','password')
-DBNAME=config.get('MySQL','dbname')
+USERNAME = config.get('MySQL', 'username')
+PW = config.get('MySQL', 'password')
+DBNAME = config.get('MySQL', 'dbname')
 
 
 def db_connect():
-	db = MySQLdb.connect("localhost",USERNAME,PW,DBNAME)
+	db = MySQLdb.connect("localhost", USERNAME, PW, DBNAME)
 	# prepare a cursor object using cursor() method
 	cursor = db.cursor()
 
-	#cursor.execute ("CREATE TABLE IF NOT EXISTS `CM2016` (`timestamp` datetime NOT NULL,  `active` tinyint(1) NOT NULL,  `program` varchar(3) NOT NULL DEFAULT '',  `step` int(2) NOT NULL,  `status` varchar(3) NOT NULL DEFAULT '',  `minutes_spent` int(11) NOT NULL,  `mV` int(11) NOT NULL,  `mA` int(11) NOT NULL,  `C` int(11) NOT NULL,  `D` int(11) NOT NULL,  `slot` varchar(1) NOT NULL DEFAULT '',  PRIMARY KEY (`timestamp`,`slot`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
+	# cursor.execute ("CREATE TABLE IF NOT EXISTS `CM2016` (`timestamp` datetime NOT NULL,  `active` tinyint(1) NOT NULL,  `program` varchar(3) NOT NULL DEFAULT '',  `step` int(2) NOT NULL,  `status` varchar(3) NOT NULL DEFAULT '',  `minutes_spent` int(11) NOT NULL,  `mV` int(11) NOT NULL,  `mA` int(11) NOT NULL,  `C` int(11) NOT NULL,  `D` int(11) NOT NULL,  `slot` varchar(1) NOT NULL DEFAULT '',  PRIMARY KEY (`timestamp`,`slot`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
 
 	return {'db': db, 'cursor': cursor}
+
 
 def db_create_table ():
 	dbx = db_connect()	
 	command = "CREATE TABLE IF NOT EXISTS `CM2016` (`timestamp` datetime NOT NULL,  `active` tinyint(1) NOT NULL,  `program` varchar(3) NOT NULL DEFAULT '',  `step` int(2) NOT NULL,  `status` varchar(3) NOT NULL DEFAULT '',  `minutes_spent` int(11) NOT NULL,  `mV` int(11) NOT NULL,  `mA` int(11) NOT NULL,  `C` int(11) NOT NULL,  `D` int(11) NOT NULL,  `slot` varchar(1) NOT NULL DEFAULT '',  PRIMARY KEY (`timestamp`,`slot`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
 	run_command (command, dbx['db'], dbx['cursor'])
 	close_db (dbx['db'])
+
 	
 """Add a CM2016 object to the database"""
+
+
 def add_CM2016(date, obj):
 	for key in obj.slots:
 			add_slot(date, obj.slots[key])
 
+
 def add_slot (date, slot):
-	if slot.is_active() == False:
+	if not slot.is_active():
 		return
 	
 	dbx = db_connect()
-	command = "INSERT INTO CM2016 VALUES ('{0}',{1},'{2}',{3},'{4}',{5},{6},{7},{8},{9},'{10}')".format (date, slot.is_active(),slot.get_program(),slot.get_step_raw(),slot.get_status(),slot.get_time_spent_in_minutes(), slot.get_voltage_in_mV(), slot.get_current_in_mA(), slot.get_charged_capacity_in_uAh(), slot.get_discharged_capacity_in_uAh(), slot.name)
+	command = "INSERT INTO CM2016 VALUES ('{0}',{1},'{2}',{3},'{4}',{5},{6},{7},{8},{9},'{10}')".format (date, slot.is_active(), slot.get_program(), slot.get_step_raw(), slot.get_status(), slot.get_time_spent_in_minutes(), slot.get_voltage_in_mV(), slot.get_current_in_mA(), slot.get_charged_capacity_in_uAh(), slot.get_discharged_capacity_in_uAh(), slot.name)
 	run_command (command, dbx['db'], dbx['cursor'])
 	close_db (dbx['db'])
 
